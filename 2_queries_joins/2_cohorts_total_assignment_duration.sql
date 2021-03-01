f1051fa8f6a106dc8da0cd470839ca3c1f6c10b5
vagrant <gio_wan@live.ca>
2021-03-01 18:46:21 +0000

    join exercises

---
+++ 2_queries_joins/2_cohorts_total_assignment_duration.sql
@@ -0,0 +1,6 @@
+SELECT sum(assignment_submissions.duration) AS total_duration
+FROM students JOIN assignment_submissions
+ON students.id = assignment_submissions.student_id
+JOIN cohorts
+ON students.cohort_id = cohorts.id
+WHERE cohorts.name = 'FEB12';
